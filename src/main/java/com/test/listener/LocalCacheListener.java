5d7f1b6bd49b1725db98ce61bbc474e512e3ecfa
xiongdw <xdw789789@163.com>
2023-07-12 17:00:03 +0800

    add

---
+++ src/main/java/com/test/listener/LocalCacheListener.java
@@ -0,0 +1,53 @@
+package com.test.listener;
+
+
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class LocalCacheListener implements ServletContextListener {
+
+    private static Logger logger = LoggerFactory.getLogger(LocalCacheListener.class);
+
+    private Map<String,Object> container = new ConcurrentHashMap<>();
+
+    /**
+     * 当Servlet 容器启动Web 应用时调用该方法。在调用完该方法之后，容器再对Filter 初始化，并且对那些在Web 应用启动时就需要被初始化的Servlet 进行初始化
+     * @param servletContextEvent
+     */
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        container.put("test",1);
+        logger.warn("启动时加载数据："+container);
+    }
+
+    /**
+     * 当Servlet 容器终止Web 应用时调用该方法。在调用该方法之前，容器会先销毁所有的Servlet 和Filter 过滤器。
+     * @param servletContextEvent
+     */
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        container.remove("test");
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public static void setLogger(Logger logger) {
+        LocalCacheListener.logger = logger;
+    }
+
+    public Map<String, Object> getContainer() {
+        return container;
+    }
+
+    public void setContainer(Map<String, Object> container) {
+        this.container = container;
+    }
+}
